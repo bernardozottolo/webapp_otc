f2d8f30e9c2a2de98adf76edb7b5ed37b6660273
Bernardo <bernardo.zottolo@homtail.com>
2026-06-17 09:57:45 -0300

    Change payment data symbol

--- src/modules/order/OrderStatusPage.tsx
+++ src/modules/order/OrderStatusPage.tsx
@@ -646,7 +646,7 @@ export function OrderStatusPage({ brand }: OrderStatusPageProps) {
                     aria-label={texts.paymentInfoTooltip}
                   >
                     <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
-                      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2Zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2Z" />
+                      <path d="M7.41 8.59 12 13.17l4.59-4.58L18 10l-6 6-6-6z" />
                     </svg>
                   </button>
                 </div>
